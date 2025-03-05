e7fae6e7559589ac93b53ece9578a5fdd8eb78a1
MikeBC9 <m.bolanosc@uniandes.edu.co>
2025-03-05 02:46:18 +0100

    Update config.py

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "Banckchurn API Mijail Bolanos"
 
     class Config:
         case_sensitive = True
